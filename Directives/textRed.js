096ff0d3b2a86550565ecc09b712f9cc05c8f03a
Fernando Fernandez <ffmp777@yahoo.com>
2016-02-08 14:19:24 -0700

    made small changes.

--- Directives/textRed.js
+++ Directives/textRed.js
@@ -1,12 +1,11 @@
 angular.module('myApp').directive('textRed', function () {
     return {
         restrict: 'A',
-        scope: {
-        },
-        link: function postLink (scope, elem, attrs) {
-            elem.hover(function() {
+        scope: {},
+        link: function postLink(scope, elem, attrs) {
+            elem.hover(function () {
                 elem.css('color', 'red')
-            }, function() {
+            }, function () {
                 elem.css('color', 'white')
             });
         }
